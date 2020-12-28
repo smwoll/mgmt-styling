f332e59d56c0737720ad162fa5e55df91892164c
Sky <skylandwoll@gmail.com>
2020-12-28 15:37:46 -0500

    mgmt start

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css"; // Import precompiled Bootstrap css
+import "@fortawesome/fontawesome-free/css/all.css";
+
+import "./styles.scss";
+
+console.log("hello world!");
